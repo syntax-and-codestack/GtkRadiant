fbeb38ca87698d5ed48ba63d09472c0a79971ac3
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-12 08:27:13 -0500

    Create mangoz-plugin-menu.h

---
+++ mangoz-plugin/mangoz-plugin-menu.h
@@ -0,0 +1,22 @@
+//mangoz-plugin-menu.h
+
+#ifndef MANGOZ_PLUGIN_MENU_H
+#define MANGOZ_PLUGIN_MENU_H
+
+//probally exceed anyways...
+#define MAX_MENU_PLUGINS 32
+
+//MENU STRINGS FOR MAIN
+const char* PLUGIN_MENU_FILE = "&File";
+const char* PLUGIN_MENU_EDIT = "&Edit";
+const char* PLUGIN_MENU_BSP = "&Bsp";
+const char* PLUGIN_MENU_SHADERS = "&Shaders";//original was textures so rename...
+const char* PLUGIN_MENU_BRUSH = "&Brush";
+const char* PLUGIN_MENU_GRID = "&Grid";
+const char* PLUGIN_MENU_FILTER = "&Filter";
+const char* PLUGIN_MENU_PATCH = "&Patch";
+const char* PLUGIN_MENU_MISC = "&Misc";
+const char* PLUGIN_MENU_PRIMITIVES = "&Primitives";
+const char* PLUGIN_MENU_HELP = "&Help";
+
+#endif
