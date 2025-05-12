66699ad087ecabf8b925d8484027546f197c51f1
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-12 10:35:07 -0500

    Update mangoz-plugin-menu.h

--- mangoz-plugin/mangoz-plugin-menu.h
+++ mangoz-plugin/mangoz-plugin-menu.h
@@ -21,56 +21,80 @@ const char* PLUGIN_MENU_MISC = "&Misc";
 const char* PLUGIN_MENU_PRIMITIVES = "&Primitives";
 const char* PLUGIN_MENU_HELP = "&Help";
 
+const char* PLUGIN_MENU[13] = {
+PLUGIN_MENU_FILE, 
+PLUGIN_MENU_EDIT,
+PLUGIN_MENU_BSP,
+PLUGIN_MENU_SHADERS,
+PLUGIN_MENU_BRUSH,
+PLUGIN_MENU_VIEW,
+PLUGIN_MENU_GRID,
+PLUGIN_MENU_FILTER,
+PLUGIN_MENU_PATCH,
+PLUGIN_MENU_PLUGIN,
+PLUGIN_MENU_MSIC,
+PLUGIN_MENU_PRIMITIVES,
+PLUGIN_MENU_HELP
+};
+
 void FlushFileMenu(){
   delete []PLUGIN_MENU_FILE;
-}
+};
 
 void FlushEditMenu(){
   delete []PLUGIN_MENU_EDIT;
-}
+};
 
 void FlushBspMenu(){
   delete []PLUGIN_MENU_BSP;
-}
+};
 
 void FlushShadersMenu(){
   delete []PLUGIN_MENU_SHADERS;
-}
+};
 
 void FlushBrushMenu(){
   delete []PLUGIN_MENU_BRUSH;
-}
+};
 
 void FlushViewMenu(){
   delete []PLUGIN_MENU_VIEW;
-}
+};
 
 void FlushGridMenu(){
   delete []PLUGIN_MENU_GRID;
-}
+};
 
 void FlushFilterMenu(){
   delete []PLUGIN_MENU_FILTER;
-}
+};
 
 void FlushPatchMenu(){
   delete []PLUGIN_MENU_PATCH;
-}
+};
 
 void FlushPluginMenu(){
   delete []PLUGIN_MENU_PLUGIN;
-}
+};
 
 void FlushMiscMenu(){
   delete []PLUGIN_MENU_MISC;
-}
+};
 
 void FlushPrimitivesMenu(){
   delete []PLUGIN_MENU_PRIMITIVES;
-}
+};
 
 void FlushHelpMenu(){
   delete []PLUGIN_MENU_HELP;
-}
+};
+
+void IncMenu(){
+  PLUGIN_MENU++;
+};
+
+void DecMenu(){
+  PLUGIN_MENU--;
+};
 
 #endif
