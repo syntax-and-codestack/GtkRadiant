363abd62531d0e0fb033a3eeed6dc75ef947d558
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-12 13:17:38 -0500

    Update mangoz-plugin-ftypes.h

--- mangoz-plugin/mangoz-plugin-ftypes.h
+++ mangoz-plugin/mangoz-plugin-ftypes.h
@@ -12,6 +12,6 @@ const char* BRUSH_FILE_EXTENSION = "*.qebrushaddress";//brush file extension
 const char* ENTITY_FILE_EXTENSION = "*.qeentityaddress";//entity file extension
 const char* REGION_FILE_EXTENSION = "*.qeregionaddress";//region file extension
 
-void FLoad( const char* pFileName, const char *pExtension );//load the file
+void FLoad( const char* pFileName, const char *pExtension );//load the file... duh
 
 #endif
