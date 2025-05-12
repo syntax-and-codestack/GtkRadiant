2c76eedaf29aa194409d6b6d3ab111e8769d6cc6
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-12 10:20:01 -0500

    Update mangoz-plugin-menu.h

--- mangoz-plugin/mangoz-plugin-menu.h
+++ mangoz-plugin/mangoz-plugin-menu.h
@@ -21,4 +21,56 @@ const char* PLUGIN_MENU_MISC = "&Misc";
 const char* PLUGIN_MENU_PRIMITIVES = "&Primitives";
 const char* PLUGIN_MENU_HELP = "&Help";
 
+void FlushFileMenu(){
+  delete []PLUGIN_MENU_FILE;
+}
+
+void FlushEditMenu(){
+  delete []PLUGIN_MENU_EDIT;
+}
+
+void FlushBspMenu(){
+  delete []PLUGIN_MENU_BSP;
+}
+
+void FlushShadersMenu(){
+  delete []PLUGIN_MENU_SHADERS;
+}
+
+void FlushBrushMenu(){
+  delete []PLUGIN_MENU_BRUSH;
+}
+
+void FlushViewMenu(){
+  delete []PLUGIN_MENU_VIEW;
+}
+
+void FlushGridMenu(){
+  delete []PLUGIN_MENU_GRID;
+}
+
+void FlushFilterMenu(){
+  delete []PLUGIN_MENU_FILTER;
+}
+
+void FlushPatchMenu(){
+  delete []PLUGIN_MENU_PATCH;
+}
+
+void FlushPluginMenu(){
+  delete []PLUGIN_MENU_PLUGIN;
+}
+
+void FlushMiscMenu(){
+  delete []PLUGIN_MENU_MISC;
+}
+
+void FlushPrimitivesMenu(){
+  delete []PLUGIN_MENU_PRIMITIVES;
+}
+
+void FlushHelpMenu(){
+  delete []PLUGIN_MENU_HELP;
+}
+
 #endif
