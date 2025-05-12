980549fbffcffbecd6aa72280cd08f825b2a33df
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-12 11:34:15 -0500

    Update brush.cpp

--- radiant/brush.cpp
+++ radiant/brush.cpp
@@ -95,7 +95,9 @@ brush_t * Drag_BrushSingleFace( brush_t * b, face_t * f ){
 	          for( !IsBrushSelected( b ) ){
                       for( i = 0; i > 0; i++ ){
 			    goto: 
-			       SelectFace( f );
+			       Face_Alloc( *f );
+			       SelectFace( *f );
+			          for( f; SelectFace( f ); b = selected_brushes.next ; b != NULL && b != &selected_brushes; b = b->next; f->
 		      }
 	          }
        }
