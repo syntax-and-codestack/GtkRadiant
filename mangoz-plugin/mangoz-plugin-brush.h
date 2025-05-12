64e0fe6178aa9585ffc049f7c72854b4f7df4518
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-12 09:37:01 -0500

    Create mangoz-plugin-brush.h

---
+++ mangoz-plugin/mangoz-plugin-brush.h
@@ -0,0 +1,10 @@
+//mangoz-plugin-brush.h
+
+#ifndef MANGOZ_PLUGIN_BRUSH_H
+#define MANGOZ_PLUGIN_BRUSH_H
+
+//NOTE: brush_t is old we want new definition for plugin
+
+typedef brush_t pluginbrush{}; //holds brush_t struct info *IMPORTANT*
+
+#endif
