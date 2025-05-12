ced2c32f76d52a6fcfddfb2839259ceb2e421d91
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-12 09:31:08 -0500

    Update mangoz-plugin-menu.h

--- mangoz-plugin/mangoz-plugin-menu.h
+++ mangoz-plugin/mangoz-plugin-menu.h
@@ -10,11 +10,13 @@
 const char* PLUGIN_MENU_FILE = "&File";
 const char* PLUGIN_MENU_EDIT = "&Edit";
 const char* PLUGIN_MENU_BSP = "&Bsp";
-const char* PLUGIN_MENU_SHADERS = "&Shaders";//original was textures so rename...
+const char* PLUGIN_MENU_SHADERS = "&Shaders"; //original was textures so rename...
 const char* PLUGIN_MENU_BRUSH = "&Brush";
+const char* PLUGIN_MENU_VIEW = "&View"; //can change XY, XZ or YZ in the menu now
 const char* PLUGIN_MENU_GRID = "&Grid";
 const char* PLUGIN_MENU_FILTER = "&Filter";
 const char* PLUGIN_MENU_PATCH = "&Patch";
+const char* PLUGIN_MENU_PLUGIN = "&Plugin"; //plugins access menu
 const char* PLUGIN_MENU_MISC = "&Misc";
 const char* PLUGIN_MENU_PRIMITIVES = "&Primitives";
 const char* PLUGIN_MENU_HELP = "&Help";
