c9d4e8975e558359c4cfe5871ff5d4e740c3be06
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-12 13:15:31 -0500

    Create mangoz-plugin-ftypes.h

---
+++ mangoz-plugin/mangoz-plugin-ftypes.h
@@ -0,0 +1,15 @@
+//mangoz-plugin-ftypes.h
+
+#ifndef MANGOZ_PLUGIN_FTYPES_H
+#define MANGOZ_PLUGIN_FTYPES_H
+
+/*
+     FILE PURPOSE:
+-save type files for radiant
+*/
+
+const char* BRUSH_FILE_EXTENSION = "*.qebrushaddress";//brush file extension
+const char* ENTITY_FILE_EXTENSION = "*.qeentityaddress";//entity file extension
+const char* REGION_FILE_EXTENSION = "*.qeregionaddress";//region file extension
+
+#endif
