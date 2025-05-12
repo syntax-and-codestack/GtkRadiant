59d29fc43463e9b56d5ff31c12385b5bad518eaf
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-12 16:18:44 -0500

    Update brushpluginz.h

--- radiant/brushpluginz.h
+++ radiant/brushpluginz.h
@@ -9,4 +9,4 @@ float fClmp( face_t * f );
 float bClmp( brush_t * b )//clamp brush! NOTE: there is already a function for this in brush.cpp, i just wanna make my own... :)
 
 //NOTE: brushes created in the 3D viewport and not in grid windows are called Fragment Brushes( there pointers are brush_t * bFrag )
-bool Mouse_DraggedViewport( CamWnd * cWnd, GLint iDragColor, vec3_t fShadeColor, brush_t * bFrag, bool bDetailed );
+bool Mouse_DraggedViewport( CamWnd * cWnd, GLInt iDragColor, vec3_t fShadeColor, brush_t * bFrag, bool bDetailed );
