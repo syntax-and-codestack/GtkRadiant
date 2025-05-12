c336f3ed083126ddf7acbed2bbc9368b9aaf02da
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-12 08:43:53 -0500

    Update qertypes.h

--- include/qertypes.h
+++ include/qertypes.h
@@ -827,7 +827,7 @@ typedef struct
 	// tells if we are internally using brush primitive (texture coordinates and map format)
 	// this is a shortcut for IntForKey( g_qeglobals.d_project_entity, "brush_primit" )
 	// NOTE: must keep the two ones in sync
-	bool bBrushPrimitMode;
+	bool Q_EBrushPrimitMode;
 
 	/*!
 	   win32: engine full path.
